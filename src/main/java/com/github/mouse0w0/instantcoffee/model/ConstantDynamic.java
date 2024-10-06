afb59b78408b009c5267d1ca0502857b2ead87ef
Mouse <mouse0w0@qq.com>
2024-10-07 00:53:46 +0800

    Rename InvokeDynamic to ConstantDynamic

--- src/main/java/com/github/mouse0w0/instantcoffee/model/ConstantDynamic.java
+++ src/main/java/com/github/mouse0w0/instantcoffee/model/ConstantDynamic.java
@@ -2,13 +2,13 @@ package com.github.mouse0w0.instantcoffee.model;
 
 import com.github.mouse0w0.instantcoffee.Location;
 
-public class InvokeDynamic extends Located {
+public class ConstantDynamic extends Located {
     public String name;
     public MethodType methodType;
     public MethodHandle bootstrapMethodHandle;
     public Object[] bootstrapMethodArguments;
 
-    public InvokeDynamic(Location location, String name, MethodType methodType, MethodHandle bootstrapMethodHandle, Object[] bootstrapMethodArguments) {
+    public ConstantDynamic(Location location, String name, MethodType methodType, MethodHandle bootstrapMethodHandle, Object[] bootstrapMethodArguments) {
         super(location);
         this.name = name;
         this.methodType = methodType;
